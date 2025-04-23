cfa54eaad34d1f10e50394d1c6fb20124118cc7b
Heytor Mesquita <dev.hmesquita26@gmail.com>
2025-04-23 03:41:46 -0300

    configuração do projeto

---
+++ src/main/java/com/testesapi/api_testes/ApiTestesApplication.java
@@ -0,0 +1,13 @@
+package com.testesapi.api_testes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTestesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTestesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testesapi/api_testes/ApiTestesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testesapi.api_testes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTestesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
