8600d51df46f340bacc1d164326a724339ee1fd9
Heytor Mesquita <dev.hmesquita26@gmail.com>
2025-04-23 03:56:16 -0300

    adiciona classe User

---
+++ src/main/java/com/testesapi/api_testes/domain/User.java
@@ -0,0 +1,20 @@
+package com.testesapi.api_testes.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
