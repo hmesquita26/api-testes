75328ef701ed4f563dbaa6b48f12ec0faf8c4a63
Heytor Mesquita <dev.hmesquita26@gmail.com>
2025-04-24 01:51:13 -0300

    adiciona UserDTO

---
+++ src/main/java/com/testesapi/api_testes/domain/dto/UserDTO.java
@@ -0,0 +1,18 @@
+package com.testesapi.api_testes.domain.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    private Integer id;
+    private String name;
+    private String email;
+    @JsonIgnore
+    private String password;
+}
