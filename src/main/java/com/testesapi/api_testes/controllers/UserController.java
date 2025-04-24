c1c2b816623dde5e6eac93093002be7836fb867c
Heytor Mesquita <dev.hmesquita26@gmail.com>
2025-04-24 01:51:48 -0300

    adiciona ModelMapper no UserController

--- src/main/java/com/testesapi/api_testes/controllers/UserController.java
+++ src/main/java/com/testesapi/api_testes/controllers/UserController.java
@@ -1,7 +1,9 @@
 package com.testesapi.api_testes.controllers;
 
 import com.testesapi.api_testes.domain.User;
+import com.testesapi.api_testes.domain.dto.UserDTO;
 import com.testesapi.api_testes.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,15 +11,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping(value = "/user")
 public class UserController {
 
     @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
     private UserService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> buscarUsuario(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(service.findById(id));
+    public ResponseEntity<UserDTO> buscarUsuario(@PathVariable Integer id) {
+        Optional<User> usuarioOpt = Optional.ofNullable(service.findById(id));
+        if (usuarioOpt.isPresent()) {
+            UserDTO dto = mapper.map(service.findById(id), UserDTO.class);
+            return ResponseEntity.ok(dto);
+        }
+        return ResponseEntity.notFound().build();
     }
 }
