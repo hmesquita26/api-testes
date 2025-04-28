fb6e19888a4d969198f826ef3e742630111aa153
Heytor Mesquita <dev.hmesquita26@gmail.com>
2025-04-28 13:57:38 -0300

    adiciona teste para classe UserServiceImpl metodo create com sucesso

--- src/test/java/com/testesapi/api_testes/services/impl/UserServiceImplTest.java
+++ src/test/java/com/testesapi/api_testes/services/impl/UserServiceImplTest.java
@@ -16,6 +16,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
@@ -88,7 +89,17 @@ class UserServiceImplTest {
     }
 
     @Test
-    void create() {
+    void whenCreateThenReturnSuccess() {
+        when(repository.save(any())).thenReturn(user);
+
+        User response = service.create(userDTO);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
     }
 
     @Test
